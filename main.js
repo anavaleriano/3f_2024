ca29177d08341a4a5d082447722bc75bba728b0d
Ana Beatriz Valeriano Araújo <101740970+anavaleriano@users.noreply.github.com>
2024-06-20 09:43:03 -0300

    20/06

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".abaconteudo");
-const contador = document. querySelectorAll(".contador")
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date (2024,11,6,21,30);
+const tempoObjetivo2 = new Date (2025,3,10,5,33);
+const tempoObjetivo3 = new Date (2025,7,16,10,50);
+const tempoObjetivo4 = new Date (2025,11,01,11,28);
+let agora = new Date();
+contador[0].textContent= tempoObjetivo1 - agora
+contador[1].textContent= tempoObjetivo2 - agora
+contador[2].textContent= tempoObjetivo3 - agora
+contador[3].textContent= tempoObjetivo4 - agora
 
-for (let i = 0; i < botoes.length; i++){
+
+for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for (let j = 0; j < botoes.length; j++){
-            botoes[j].classList.remove("ativo")
-            conteudo[j].classList.remove("ativo")
-            
-        }
-        botoes[i].classList.add("ativo")
-        conteudo[i].classList.add("ativo")
+     for (let j = 0; j< botoes.length ; j++){
+         botoes[j].classList.remove("ativo")
+         conteudo[j].classList.remove("ativo")
+     }   
+     botoes[i].classList.add("ativo")
+     conteudo[i].classList.add("ativo")
     }
 }
-
